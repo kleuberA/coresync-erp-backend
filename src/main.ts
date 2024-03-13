2b286b38bb7fa427dc8c341b0bb68061f3bc2246
Kleuber Silva <kleuber20@hotmail.com>
2024-03-13 11:41:03 -0300

    Add Swagger UI theme to the API documentation.

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { ValidationPipe } from '@nestjs/common';
 import * as cookieParser from 'cookie-parser';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerTheme, SwaggerThemeName } from 'swagger-themes';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -24,9 +25,12 @@ async function bootstrap() {
     .setTitle('CoreSync ERP API')
     .setDescription('The CoreSync ERP API, for all your ERP needs.')
     .setVersion('1.0')
+    .addBearerAuth()
     .build();
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
+  const theme = new SwaggerTheme();
+  const optionsV1 = theme.getDefaultConfig('dark' as SwaggerThemeName); // Fix: Cast the argument to SwaggerThemeName
+  SwaggerModule.setup('api', app, document, optionsV1);
 
   await app.listen(3000);
 }
