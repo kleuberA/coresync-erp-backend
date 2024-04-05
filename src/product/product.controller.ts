f7dccd565b01882c003cb34dac0738ad2324cf6c
Kleuber Silva <kleuber20@hotmail.com>
2024-04-05 10:47:40 -0300

    "Add pagination to product service and controller; update module dependencies"
    This diff adds pagination to the product service and controller by using the `createPaginator` function and the `GetProductFilter` type. The `ProductService` has been updated to include a `where` condition based on the provided filters, and the `@Get` method in the `ProductController` now accepts a `GetProductFilter` object as a query parameter. Additionally, the `ProductModule` has been updated to include the `ProductService` and `PrismaService` as providers.

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -1,17 +1,23 @@
 import { ProductService } from './product.service';
-import { Controller, Get, HttpStatus, Param, Req, Res } from '@nestjs/common';
+import { Controller, Get, HttpStatus, Param, Query, Req, Res } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
 import { Request, Response } from 'express';
+import { IsPublic } from 'src/auth/decorators/ispublic.decorator';
+import { ApiPaginatedResponse } from 'src/common/decorators/ApiPaginatedResponse';
+import { ProjectOutputDto } from 'src/project/DTO/project-dto';
+import { GetProductFilter } from './GetProductFilter';
 
 @ApiTags('Product')
 @Controller('product')
 export class ProductController {
     constructor(private readonly productService: ProductService) { }
 
+    @IsPublic()
+    @ApiPaginatedResponse(ProjectOutputDto)
     @Get()
-    async getAllProducts(@Res() res: Response) {
+    async getAllProducts(@Res() res: Response, @Query() filters: GetProductFilter) {
         try {
-            const products = await this.productService.getAllProducts();
+            const products = await this.productService.getAllProducts(filters);
             return res.status(HttpStatus.OK).json({ message: 'Products fetched successfully!', data: products })
         } catch (error) {
             return res.status(HttpStatus.BAD_REQUEST).json({ message: 'Failed to fetch products!', error: error.message })
