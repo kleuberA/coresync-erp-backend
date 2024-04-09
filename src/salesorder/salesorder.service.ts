7efa5a7a8eb436e2ccfd560e3a6c5b4f3dc46122
Kleuber Silva <kleuber20@hotmail.com>
2024-04-09 10:43:07 -0300

    "Add salesorder controller and service for fetching sales order by id; throw errors if company or sales order not found"
    This diff adds a new controller and service for handling requests related to fetching sales orders by a given id and company id. The controller has a new route `GET /salesorder/:id/:companyId` which calls the service method `getSalesOrderById`. This method checks if the company and sales order exist in the database, and if not, throws an error. If both the company and sales order are found, the method returns the sales order.

--- src/salesorder/salesorder.service.ts
+++ src/salesorder/salesorder.service.ts
@@ -1,4 +1,30 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { SalesOrder } from './entity/salesorder.entity';
 
 @Injectable()
-export class SalesorderService {}
+export class SalesorderService {
+    constructor(private readonly prisma: PrismaService) { }
+
+    async getSalesOrderById(salesOrderId: string, companyId: string): Promise<SalesOrder> {
+
+        const existCompany = await this.prisma.company.findUnique({
+            where: { id: companyId },
+            select: { id: true }
+        })
+
+        if (!existCompany) throw new Error('Company not found');
+
+        const existSalesOrder = await this.prisma.salesOrder.findUnique({
+            where: {
+                id: salesOrderId,
+                companyID: companyId
+            }
+        })
+
+        if (!existSalesOrder) throw new Error('Sales Order not found');
+
+        return existSalesOrder;
+    }
+
+}
