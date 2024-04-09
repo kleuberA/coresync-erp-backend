fc54db26585fab3d03ee4f20ac8895926ec12257
Kleuber Silva <kleuber20@hotmail.com>
2024-04-09 10:59:18 -0300

    "Add delete sales order endpoint and related service; add permission check for delete operation"
    This diff adds a new delete endpoint for the Sales Order controller and the corresponding service method to delete a sales order. Also, it includes a permission check to verify if the user is authorized to delete a sales order.

--- src/salesorder/salesorder.controller.ts
+++ src/salesorder/salesorder.controller.ts
@@ -1,6 +1,6 @@
 import { ApiTags } from '@nestjs/swagger';
 import { SalesorderService } from './salesorder.service';
-import { Controller, Get, HttpStatus, Param, Res } from '@nestjs/common';
+import { Controller, Delete, Get, HttpStatus, Param, Res } from '@nestjs/common';
 import { Response } from 'express';
 
 @ApiTags("Sales Order")
@@ -15,6 +15,16 @@ export class SalesorderController {
             return res.status(HttpStatus.OK).json({ message: 'Sales Order fetched successfully!', data: salesOrder });
         } catch (error) {
             return res.status(HttpStatus.BAD_REQUEST).json({ message: "Failed to fetch sales order!", error: error.message });
+        }
+    }
+
+    @Delete("/delete/:id/:companyId/:userId")
+    async deleteSalesOrder(@Res() res: Response, @Param('id') salesOrderId: string, @Param('companyId') companyId: string, @Param('userId') userId: string) {
+        try {
+            const salesOrder = await this.salesorderService.deleteSalesOrder(salesOrderId, companyId, userId);
+            return res.status(HttpStatus.OK).json({ message: 'Sales Order deleted successfully!', data: salesOrder });
+        } catch (error) {
+            return res.status(HttpStatus.BAD_REQUEST).json({ message: "Failed to delete sales order!", error: error.message });
         }
     }
 
--- src/salesorder/salesorder.service.ts
+++ src/salesorder/salesorder.service.ts
@@ -1,6 +1,6 @@
-import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { SalesOrder } from './entity/salesorder.entity';
+import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class SalesorderService {
@@ -25,6 +25,46 @@ export class SalesorderService {
         if (!existSalesOrder) throw new Error('Sales Order not found');
 
         return existSalesOrder;
+    }
+
+    async deleteSalesOrder(salesOrderId: string, companyId: string, userId: string): Promise<SalesOrder> {
+
+        const existSalesOrder = await this.prisma.salesOrder.findUnique({
+            where: {
+                id: salesOrderId,
+                companyID: companyId
+            }
+        })
+
+        if (!existSalesOrder) throw new Error('Sales Order not found');
+
+        await this.permissionUser(userId, companyId, 'delete');
+
+        const deleteSalesOrder = await this.prisma.salesOrder.delete({
+            where: {
+                id: salesOrderId
+            }
+        });
+
+        return deleteSalesOrder;
+    }
+
+    async permissionUser(userId: string, companyId: string, method: string) {
+        const userPermission = await this.prisma.user.findFirst({
+            where: {
+                id: userId,
+                companyId: companyId
+            },
+            select: {
+                roles: {
+                    where: {
+                        name: 'admin_company',
+                    }
+                }
+            }
+        });
+
+        if (!userPermission) throw new Error(`User not authorized to ${method} task.`);
     }
 
 }
