36433d85b058413e41fa26cef21e3723c0169136
Kleuber Silva <kleuber20@hotmail.com>
2024-04-13 14:20:25 -0300

    "Added @nestjs/swagger ApiProperty decorators to DTOs in product and salesorder modules"

--- src/product/DTO/create-product-dto.ts
+++ src/product/DTO/create-product-dto.ts
@@ -1,59 +1,87 @@
 import { IsDateString, IsString, MaxLength, MinLength } from "class-validator";
+import { ApiProperty } from "@nestjs/swagger";
 
 export class CreateProduct {
 
+    @ApiProperty({
+        description: 'Name of the product',
+        type: 'string',
+        required: true,
+        example: 'Product Name'
+    })
     @IsString()
     @MinLength(3, { message: 'Name is too short' })
     @MaxLength(50, { message: 'Name is too long' })
     name: string;
 
+    @ApiProperty({
+        description: 'Short description of the product',
+        type: 'string',
+        required: true,
+        example: 'Short Description'
+    })
     @IsString()
     @MinLength(3, { message: 'Detailed description is too short' })
     @MaxLength(200, { message: 'Detailed description is too long' })
     detailedDescription: string;
 
+    @ApiProperty()
     @IsString()
     unitOfMeasure: string;
 
+    @ApiProperty()
     @IsString()
     weight: string;
 
+    @ApiProperty()
     @IsString()
     dimensions: string;
 
+    @ApiProperty()
     @IsString()
     stock: string;
 
+    @ApiProperty()
     @IsDateString()
     manufactureDate: Date;
 
+    @ApiProperty()
     @IsDateString()
     expirationDate: Date;
 
+    @ApiProperty()
     @IsString()
     purchase: string;
 
+    @ApiProperty()
     @IsString()
     selling: string;
 
+    @ApiProperty()
     @IsString()
     profit: string;
 
+    @ApiProperty()
     @IsString()
     minimumOrderQuantity: string;
 
+    @ApiProperty()
     @IsString()
     minimumStockQuantity: string;
 
+    @ApiProperty()
     @IsString()
     maximumStockQuantity: string;
 
+    @ApiProperty()
     @IsString()
     replenishment: string;
 
+    @ApiProperty()
     @IsString()
     supplierId: string;
 
+    @ApiProperty()
     @IsString()
     userId: string;
 }
