c4c595f5495b3919b4124cf5a42c57b25eee9ba4
Kleuber Silva <kleuber20@hotmail.com>
2024-04-26 09:54:53 -0300

    "Add method to get stock by id in StockService and corresponding endpoint in StockController"
    This diff adds a new method `getStockById` to the `StockService` which retrieves a stock by its id from the pr

--- src/stock/stock.controller.ts
+++ src/stock/stock.controller.ts
@@ -1,5 +1,5 @@
 import { ApiPaginatedResponse } from 'src/common/decorators/ApiPaginatedResponse';
-import { Controller, Get, HttpStatus, Query, Res } from '@nestjs/common';
+import { Controller, Get, HttpStatus, Param, Query, Res } from '@nestjs/common';
 import { GetStockFilter } from './interfaces/GetStockFilter';
 import { StockOutputDTO } from './DTO/stock-dto';
 import { StockService } from './stock.service';
@@ -16,6 +16,16 @@ export class StockController {
     async getStocks(@Res() res: Response, @Query() filters: GetStockFilter) {
         try {
             const stock = await this.stockService.getStocks(filters);
+            return res.status(HttpStatus.OK).json({ message: 'Stock fetched successfully!', data: stock });
+        } catch (error) {
+            return res.status(HttpStatus.BAD_REQUEST).json({ message: 'An error occurred while fetching stock.', error: error.message });
+        }
+    }
+
+    @Get('/:id')
+    async getStockById(@Res() res: Response, @Param('id') id: string) {
+        try {
+            const stock = await this.stockService.getStockById(id);
             return res.status(HttpStatus.OK).json({ message: 'Stock fetched successfully!', data: stock });
         } catch (error) {
             return res.status(HttpStatus.BAD_REQUEST).json({ message: 'An error occurred while fetching stock.', error: error.message });
